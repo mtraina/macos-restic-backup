eb68ba054d9f6cd08aa1cea9b007694b90ec614e
mtraina <matteo.traina.mail@gmail.com>
2022-08-27 15:06:06 +0200

    script II

--- backup-eu.sh
+++ backup-eu.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+source "backup-eu.cfg"
 
-export RESTIC_PASSWORD=# fill it
-export B2_ACCOUNT_ID=# fill it
-export B2_ACCOUNT_KEY=# fill it
+export RESTIC_PASSWORD=$RESTIC_PASSWORD
+export B2_ACCOUNT_ID=$B2_ACCOUNT_ID
+export B2_ACCOUNT_KEY=$B2_ACCOUNT_KEY
 
 /usr/local/bin/restic -v -r b2:matteo-backup-eu backup /Users/matteo/Documents -o b2.connections=10
 /usr/local/bin/restic -v -r b2:matteo-backup-eu forget --keep-last 10
 
-export RESTIC_PASSWORD=# leave it blank
-export B2_ACCOUNT_ID=# leave it blank
-export B2_ACCOUNT_KEY=# leave it blank
+export RESTIC_PASSWORD=
+export B2_ACCOUNT_ID=
+export B2_ACCOUNT_KEY=
 
 exit 0
