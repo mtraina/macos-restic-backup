3d4878ad4a1219aaf26f0dada07072591d3edc6d
mtraina <matteo.traina.mail@gmail.com>
2022-08-27 15:27:43 +0200

    simplification

--- eu/backup-eu.sh
+++ eu/backup-eu.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 source "backup-eu.cfg"
 
-export RESTIC_PASSWORD=$RESTIC_PASSWORD
-export B2_ACCOUNT_ID=$B2_ACCOUNT_ID
-export B2_ACCOUNT_KEY=$B2_ACCOUNT_KEY
-
 /usr/local/bin/restic -v -r b2:matteo-backup-eu backup /Users/matteo/Documents -o b2.connections=10
 /usr/local/bin/restic -v -r b2:matteo-backup-eu forget --keep-last 10
 
--- uswest/backup-uswest.sh
+++ uswest/backup-uswest.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 source "backup-uswest.cfg"
 
-export RESTIC_PASSWORD=$RESTIC_PASSWORD
-export B2_ACCOUNT_ID=$B2_ACCOUNT_ID
-export B2_ACCOUNT_KEY=$B2_ACCOUNT_KEY
-
 /usr/local/bin/restic -v -r b2:matteo-backup-uswest backup /Users/matteo/Documents -o b2.connections=10
 /usr/local/bin/restic -v -r b2:matteo-backup-uswest forget --keep-last 10
 
